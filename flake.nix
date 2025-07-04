7768e7638d83df20bd1c86564c7111c7c35c51a7
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-07-05 00:07:21 +0200

    refactor(flake): Switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -2,47 +2,51 @@
   description = "Flake for building and working with Lilypond";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     lmi.url = "github:niveK77pur/lilypond-midi-input";
     naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    lmi,
-    naersk,
-  }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      naersk' = pkgs.callPackage naersk {};
-    in {
-      devShell = pkgs.mkShell {
-        packages = [
-          # working with lilypond
-          pkgs.lilypond
-          pkgs.python311Packages.python-ly
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        naersk' = pkgs.callPackage inputs.naersk {};
+      in {
+        devshells.default = {
+          packages = [
+            # working with lilypond
+            pkgs.lilypond
+            pkgs.python311Packages.python-ly
 
-          # handy tools
-          pkgs.neovim-remote
-          pkgs.timidity
-          lmi.defaultPackage.${system}
-          (naersk'.buildPackage {
-            src = pkgs.fetchFromGitLab {
-              owner = "dajoha";
-              repo = "midiplay";
-              rev = "f0e13a7872ba914772e3d3208333dc6c72c0c37c";
-              hash = "sha256-Q6iTx2QUDk/VZb76sWguLT2LhdprjHaWCqxiMmiGZFM=";
-            };
-            nativeBuildInputs = with pkgs; [pkg-config alsa-lib ncurses];
-          })
+            # handy tools
+            pkgs.neovim-remote
+            pkgs.timidity
+            inputs.lmi.defaultPackage.${system}
+            (naersk'.buildPackage {
+              src = pkgs.fetchFromGitLab {
+                owner = "dajoha";
+                repo = "midiplay";
+                rev = "f0e13a7872ba914772e3d3208333dc6c72c0c37c";
+                hash = "sha256-Q6iTx2QUDk/VZb76sWguLT2LhdprjHaWCqxiMmiGZFM=";
+              };
+              nativeBuildInputs = with pkgs; [pkg-config alsa-lib ncurses];
+            })
 
-          # viewers
-          pkgs.zathura
-          pkgs.kdePackages.okular
-        ];
+            # viewers
+            pkgs.zathura
+            pkgs.kdePackages.okular
+          ];
+        };
       };
-    });
+    };
 }
